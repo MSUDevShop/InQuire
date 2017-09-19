a8858517eb7a278af4057cf1c9efa9edc8c7c411
yashvesikar <yashvesikar@gmail.com>
2017-09-18 21:44:16 -0400

    -mUpdating upstream

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+// import {Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { FormsModule }   from '@angular/forms'
 import { Angular2Apollo } from 'angular2-apollo';
 import gql from 'graphql-tag';
 import 'rxjs/add/operator/toPromise';
@@ -11,8 +13,17 @@ import 'rxjs/add/operator/toPromise';
 })
 export class HomePage {
   questions = <any>[];
+  question: "";
+  value: "";
+  userQuestion: {
+    question;
+    value;
+  };
 
-  constructor(public navCtrl: NavController, public apollo: Angular2Apollo) {
+  constructor(public navCtrl: NavController,
+              public apollo: Angular2Apollo,
+              //  private formBuilder: FormBuilder
+             ) {
     this.apollo.query({
       query: gql`
         query {
@@ -26,6 +37,26 @@ export class HomePage {
       this.questions = data;
       this.questions = this.questions.allQuestions;
     })
+    // this.userQuestion = this.formBuilder.group({
+    //   question: ['', Validators.required],
+    //   questionValue: []
+    // });
+
+  }
+
+  submitQuestion() {
+    this.apollo.mutate({
+      mutation: gql`
+      mutation($question:String!) {
+      	createQuestion(question: $question){
+          question
+        }
+      }
+      `, variables:{
+        question: this.userQuestion.question
+      }
+    });
+    console.log(this.userQuestion.question);
   }
 
 }
