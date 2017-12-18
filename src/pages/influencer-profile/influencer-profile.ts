36c0a73596c4a3de62f85f5481588ca48e165812
Gustavo Fulton <ferna194@msu.edu>
2017-12-18 17:00:27 -0500

    changes to home and influencer profile

--- src/pages/influencer-profile/influencer-profile.ts
+++ src/pages/influencer-profile/influencer-profile.ts
@@ -61,6 +61,7 @@ export class InfluencerProfilePage {
             profilePic
             fullName
             isInfluencer
+            backgroundPic
             questions {
               id
               question
